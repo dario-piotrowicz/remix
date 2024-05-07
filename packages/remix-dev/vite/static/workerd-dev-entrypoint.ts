3cf992602af824ab293509e813ea784075ab2f0a
Dario Piotrowicz <dario@cloudflare.com>
2024-05-07 16:34:48 +0100

    change the workerd-dev-entrypoint export

--- packages/remix-dev/vite/static/workerd-dev-entrypoint.ts
+++ packages/remix-dev/vite/static/workerd-dev-entrypoint.ts
@@ -8,15 +8,17 @@ const build = await import("virtual:remix/server-build") as ServerBuild;
 
 const remixHandler = createRequestHandler(build, "development");
 
-export default function entrypointHandler(req: Request, env: any, ctx: any): Promise<Response> {
-    let loadContext = {
-        cloudflare: {
-            env,
-            cf: (req as any).cf,
-            ctx,
-            // @ts-ignore
-            caches,
-        }
-    };
-    return remixHandler(req, loadContext);
+export default {
+    fetch(req: Request, env: any, ctx: any): Promise<Response> {
+        let loadContext = {
+            cloudflare: {
+                env,
+                cf: (req as any).cf,
+                ctx,
+                // @ts-ignore
+                caches,
+            }
+        };
+        return remixHandler(req, loadContext);
+    }
 }
